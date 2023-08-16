9a163ff4390d9b0d1f4cc35e399a82427cca6394
swoops503 <james.roundy4@gmail.com>
2023-08-15 20:55:36 -0700

    added spacing between the links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to={"./pages/Posts"}>Posts</Link>
-      <Link to={"/pages/Profile"}>Profile</Link>
-      <Link to={"/pages/Login"}>Login</Link>
-      <Link to={"/pages/Register"}>Register</Link>
+      <Link to={"./pages/Posts"}>Posts </Link>|
+      <Link to={"/pages/Profile"}> Profile </Link>|
+      <Link to={"/pages/Login"}> Login </Link>|
+      <Link to={"/pages/Register"}> Register</Link>
 
     </div>
   );
